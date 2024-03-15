1abfa14c603f7097d74bf42334b8d4dc1730babf
Gdn <giri1208srinivas@gmail.com>
2024-03-15 13:47:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=["*"],
+    allow_headers=['X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'],
 )
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
