7f3fdbfbf55326837b52809310f7f7de26f7135e
Gdn <giri1208srinivas@gmail.com>
2024-03-15 10:48:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,10 +30,11 @@ app = FastAPI()
 # Allow requests from localhost:3000
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
+    expose_headers=["*"]
 )
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
