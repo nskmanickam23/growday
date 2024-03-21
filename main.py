ee7b414d2f5954948b6a25989884c5cc9f195ad3
giridharababu <giri1208srinivas@gmail.com>
2024-03-21 11:57:59 +0530

    addded validations and business logic

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from routes.emails import *
 from database.database import database
 # auth
 from fastapi.security import (OAuth2PasswordBearer)
+from bson.json_util import dumps, loads
 
 # templates
 from fastapi.templating import Jinja2Templates
@@ -24,6 +25,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 # CORS headers
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
@@ -51,6 +53,15 @@ app.include_router(members.members_router,  tags=["members"])
 app.include_router(customer.customer_router,  tags=["customer"])
 app.include_router(business_register.business_router, tags=["business"])
 
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            return str(obj)
+        elif isinstance(obj, datetime):
+            return obj.isoformat()
+        else:
+            return super().default(obj)
+
 
 @app.get("/<collection_name>/list")
 def list_customers(collection_name, token: str = Depends(val_token)):
@@ -60,12 +71,39 @@ def list_customers(collection_name, token: str = Depends(val_token)):
         if database.collection_exists(collection_name):
             list_collections = database.get_collection(collection_name)
             # Retrieve all documents from the collection
-            print(list_collections)
-            records = []
-            for document in list_collections.find({}):
-                document = json.loads(json_util.dumps(document))
-                records.append(document)
-            return {"records": records}
+            user_collection = database.get_collection('users')
+            find_user = user_collection.find_one({'email': token[1]['email']})
+            if find_user:
+                search_criteria = {
+                    "User_ids": {
+                        "$elemMatch": {
+                            "$elemMatch": {
+                                "$in": [
+                                    find_user['_id']
+                                ]
+                            }
+                        }
+                    }
+                }
+                print(search_criteria)
+
+                # Find documents matching the search criteria
+                cursor = list_collections.find(search_criteria)
+                # # Iterate over the results
+                # document_list = []
+                # for document in cursor:
+                #     document_list.append(document)
+                # # print(list_collections)
+                # records = []
+                # for document in list_collections.find({}):
+                #     document = json.loads(json_util.dumps(document))
+                #     records.append(document)
+                # print("---",business_list)
+                # print(list(cursor))
+                documents_list = list(cursor)
+                from fastapi.responses import JSONResponse
+
+                return json.loads(json_util.dumps(documents_list))
         else:
             raise HTTPException(status_code=404, detail='Collection Not Found')
     else:
